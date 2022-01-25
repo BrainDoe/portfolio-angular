28b83d97aba37297e18631dd3df65bb295baa42a
Donatus Brain Okwe <braindona@gmail.com>
2022-01-25 08:08:32 -0600

    Initial commit of the angular portfolio website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroductionComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
